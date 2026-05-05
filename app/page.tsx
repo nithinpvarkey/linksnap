6f4aeee6e5bcd81f74e211bd09ee0c4ea29c2e9d
nithinvarkey <nithinpv008@gmail.com>
2026-05-05 17:32:48 +0530

    chore: reset page.tsx to clean placeholder before homepage build

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,10 @@
-'use client'
-
-import { LinkCard } from '@/components/LinkCard'
+import { SkeletonCard } from '@/components/SkeletonCard'
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-slate-50 p-8">
       <div className="max-w-lg mx-auto">
-        <LinkCard
-          url="https://www.bbc.com/news"
-          isPro={false}
-          onUpgradeNeeded={() => console.log('upgrade')}
-        />
+        <SkeletonCard />
       </div>
     </main>
   )
